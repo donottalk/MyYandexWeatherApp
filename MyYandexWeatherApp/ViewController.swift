54512c3f34bac93a5fe947833d2c52c88eae245d
Anton Dobrovinskiy <pochtaantona@gmail.com>
2020-07-22 17:42:44 +0300

    Initial Commit

---
+++ MyYandexWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyYandexWeatherApp
+//
+//  Created by Антон Добровинский on 22/7/20.
+//  Copyright © 2020 Антон Добровинский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
